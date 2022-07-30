777bc108ffe5a9d2fb573a91d26d304b8506bdfc
Jrodolfosilva <100287751+Jrodolfosilva@users.noreply.github.com>
2022-07-30 05:18:35 -0300

    Update Description
    Captura do params name da url que será usado para renderizar os dados do pais na page de detalhes

--- src/pages/Description.tsx
+++ src/pages/Description.tsx
@@ -1,13 +1,15 @@
 import React from "react";
-
+import {useParams} from "react-router-dom"
 
 
 //posso passar o ValidateSearch para a Home/App/Root renderizar e dela passar como element do Route ou procurá usar o useContext 
 
 const Description= ()=>{
+    const {name} = useParams()
+    
     return(
         <div>
-            <h1>Detalhes sobre o pais selecionado</h1>
+            <h1>O pais que vocẽ deseja mais detalhes é o: {name}</h1>
         </div>
     )
 }
